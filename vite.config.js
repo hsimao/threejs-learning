a6010ce3ae74296e5b6ea16eeb60b8a5ff892ee3
hsimao <e087754958@gmail.com>
2022-09-28 19:18:54 +0800

    feat: 16 - shadows

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         "05-transform": resolve(__dirname, "05-transform/index.html"),
         "06-group": resolve(__dirname, "06-group/index.html"),
-        "07-animation": resolve(__dirname, "07-animation/index.html")
+        "07-animation": resolve(__dirname, "07-animation/index.html"),
+        "16-shadows": resolve(__dirname, "16-shadows/index.html")
       }
     }
   }
