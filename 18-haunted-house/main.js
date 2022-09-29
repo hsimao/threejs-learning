178e24d138f9dc6c2f63890e2ee725bb8f7b508a
hsimao <e087754958@gmail.com>
2022-09-29 23:08:48 +0800

    feat: house 增加陰影效果

--- 18-haunted-house/main.js
+++ 18-haunted-house/main.js
@@ -186,6 +186,7 @@ for (let i = 0; i < 50; i++) {
   grave.position.set(x, 0.3, z);
   grave.rotation.y = (Math.random() - 0.5) * 0.4;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.castShadow = true;
   graves.add(grave);
 }
 
@@ -233,6 +234,18 @@ doorLight.position.set(0, 2.2, 2.7);
 house.add(doorLight);
 
 /**
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight("#ff00ff", 2, 3);
+scene.add(ghost1);
+
+const ghost2 = new THREE.PointLight("#00ffff", 2, 3);
+scene.add(ghost2);
+
+const ghost3 = new THREE.PointLight("#ffff00", 2, 3);
+scene.add(ghost3);
+
+/**
  * Materials
  */
 const material = new THREE.MeshStandardMaterial();
@@ -272,9 +285,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.x = 1;
-camera.position.y = 1;
-camera.position.z = 2;
+camera.position.x = 0;
+camera.position.y = 7;
+camera.position.z = 10;
 scene.add(camera);
 
 // Controls
@@ -291,11 +304,44 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor(BG_COLOR);
 
+/**
+ * Shadows
+ */
 // 啟用陰影
-renderer.shadowMap.enabled = false;
-
+renderer.shadowMap.enabled = true;
 // 陰影類型
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+
+// 地板接收陰影
+floor.receiveShadow = true;
+
+// 限制作用範圍來優化效能, 提升陰影作用效果
+doorLight.shadow.mapSize.width = 256;
+doorLight.shadow.mapSize.height = 256;
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
 
 /**
  * Animate
@@ -304,6 +350,25 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update ghosts
+  const ghost1Angle = elapsedTime * 0.5;
+  // 圍繞著中心轉圈圈
+  ghost1.position.x = Math.cos(ghost1Angle) * 4;
+  ghost1.position.z = Math.sin(ghost1Angle) * 4;
+  ghost1.position.y = Math.sin(ghost1Angle * 3);
+
+  const ghost2Angle = elapsedTime * 0.32;
+  // 圍繞著中心轉圈圈
+  ghost2.position.x = Math.cos(ghost2Angle) * 5;
+  ghost2.position.z = Math.sin(ghost2Angle) * 5;
+  ghost2.position.y = Math.sin(ghost2Angle * 4) + Math.sin(elapsedTime * 2.5);
+
+  const ghost3Angle = elapsedTime * 0.18;
+  ghost3.position.x =
+    Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5));
+  ghost3.position.y = Math.sin(elapsedTime * 5) + Math.sin(elapsedTime * 2.5);
 
   // Update controls
   controls.update();
