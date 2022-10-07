c19fe9d6e0774ff397138cbf47c931c441f38b61
hsimao <e087754958@gmail.com>
2022-10-07 17:37:41 +0800

    feat: 23 physics

--- 23-physics/main.js
+++ 23-physics/main.js
@@ -2,11 +2,31 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import CANNON from "cannon";
 
 /**
  * Debug
  */
 const gui = new dat.GUI();
+const debugObject = {
+  createSphere: () => {
+    createSphere(Math.random() * 0.5, {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    });
+  },
+  createBox: () => {
+    createBox(Math.random(), Math.random(), Math.random(), {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    });
+  },
+};
+
+gui.add(debugObject, "createSphere");
+gui.add(debugObject, "createBox");
 
 /**
  * Base
@@ -24,29 +44,74 @@ const textureLoader = new THREE.TextureLoader();
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 const environmentMapTexture = cubeTextureLoader.load([
-  "/textures/environmentMaps/0/px.png",
-  "/textures/environmentMaps/0/nx.png",
-  "/textures/environmentMaps/0/py.png",
-  "/textures/environmentMaps/0/ny.png",
-  "/textures/environmentMaps/0/pz.png",
-  "/textures/environmentMaps/0/nz.png",
+  "./textures/environmentMaps/0/px.png",
+  "./textures/environmentMaps/0/nx.png",
+  "./textures/environmentMaps/0/py.png",
+  "./textures/environmentMaps/0/ny.png",
+  "./textures/environmentMaps/0/pz.png",
+  "./textures/environmentMaps/0/nz.png",
 ]);
 
 /**
- * Test sphere
+ * Physics
  */
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 32, 32),
-  new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
-  })
+// World
+const world = new CANNON.World();
+world.gravity.set(0, -9.92, 0);
+
+// 物理材質 Materials, 不同材質決定了碰撞反彈大小
+// 塑膠材質
+// const plasticMaterial = new CANNON.Material("plastic");
+// 混泥土材質
+const defaultMaterial = new CANNON.Material("concrete");
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    // 摩擦
+    friction: 0.1,
+    // 回彈值, 設定 1 彈回的高度不會變, 越小回彈幅度越小, 反之越大
+    restitution: 0.7,
+  }
 );
-sphere.castShadow = true;
-sphere.position.y = 0.5;
-scene.add(sphere);
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
+
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 3, 0),
+  shape: sphereShape,
+});
+
+// // 推擠效果
+// sphereBody.applyLocalForce(
+//   // 從 x 軸 250 方向往 x 軸 0 衝撞
+//   new CANNON.Vec3(150, 0, 0),
+//   new CANNON.Vec3(0, 0, 0)
+// );
+
+// world.addBody(sphereBody);
+
+// Floor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+
+// 靜止不動的地板
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+
+// 地板方向旋轉成跟 three 地板一致
+floorBody.quaternion.setFromAxisAngle(
+  // 旋轉軸
+  new CANNON.Vec3(-1, 0, 0),
+  // 旋轉角度
+  Math.PI * 0.5
+);
+
+world.addBody(floorBody);
 
 /**
  * Floor
@@ -142,12 +207,97 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Utils
+ */
+const objectsToUpdate = [];
+
+// Sphere
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createSphere = (radius, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+  mesh.scale.set(radius, radius, radius);
+  // 陰影
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // Cannon.js body
+  const shape = new CANNON.Sphere(radius);
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  // Save in objects to update
+  objectsToUpdate.push({ mesh, body });
+};
+
+createSphere(0.5, { x: 0, y: 3, z: 0 });
+
+// Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createBox = (width, height, depth, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+  mesh.scale.set(width, height, depth);
+  // 陰影
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // Cannon.js body
+  // cannon box 是從中心開始往四方延長計算, 所以長寬高給一半值即可
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  );
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  // Save in objects to update
+  objectsToUpdate.push({ mesh, body });
+};
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
+let oldElapsedTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+
+  // Update physics world
+  world.step(1 / 60, deltaTime, 3);
+  objectsToUpdate.forEach((object) => {
+    object.mesh.position.copy(object.body.position);
+  });
 
   // Update controls
   controls.update();
